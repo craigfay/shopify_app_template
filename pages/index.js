533945ebbe907d36d1f0c6aa10d37452c91e7a3f
Craig Fay <craigfay1@gmail.com>
2019-06-03 16:55:02 -0500

    Populate example pages

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,25 @@
-import { TextStyle } from '@shopify/polaris';
+import { EmptyState, Layout, Page, TextStyle } from '@shopify/polaris';
+const img = 'https://cdn.shopify.com/s/files/1/0757/9955/files/empty-state.svg';
 
 const Index = () => (
-  <div>
-    <TextStyle variation="positive">
-      Sample app using React and Next.js
-    </TextStyle>
-  </div>
+  <Page
+    primaryAction={{
+      content: 'Select products',
+    }}
+  >
+    <Layout>
+      <EmptyState
+          heading="Discount your products temporarily"
+          action={{
+            content: 'Select products',
+            onAction: () => console.log('clicked'),
+          }}
+          image={img}
+        >
+          <p>Select products to change their price temporarily.</p>
+        </EmptyState>
+    </Layout>
+  </Page>
 );
 
 export default Index;
