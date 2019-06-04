c4230ee91013e82b366884f57ab9fd58d73e8b02
Craig Fay <craigfay1@gmail.com>
2019-06-04 10:13:36 -0500

    Introduce a resource picker

---
+++ pages/resources.js
@@ -0,0 +1,43 @@
+import { EmptyState, Layout, Page, TextStyle, ResourcePicker } from '@shopify/polaris';
+const img = 'https://cdn.shopify.com/s/files/1/0757/9955/files/empty-state.svg';
+
+class Resources extends React.Component {
+  state = { open: false };
+  render() {
+    return (
+      <Page
+        primaryAction={{
+          content: 'Select products',
+          onAction: () => this.setState({ open: true }),
+        }}
+      >
+        <ResourcePicker
+          resourceType="Product"
+          showVariants={false}
+          open={this.state.open}
+          onSelection={(resources) => this.handleSelection(resources)}
+          onCancel={() => this.setState({ open: false })}
+        />
+        <Layout>
+          <EmptyState
+            heading="Select products to start"
+            action={{
+              content: 'Select products',
+              onAction: () => this.setState({ open: true }),
+            }}
+            image={img}
+          >
+            <p>Select products and change their price temporarily</p>
+          </EmptyState>
+        </Layout>
+      </Page>
+    );
+  }
+  handleSelection = (resources) => {
+    const idsFromResources = resources.selection.map((product) => product.id);
+    this.setState({ open: false })
+    console.log(resources)
+  };
+}
+
+export default Resources;
