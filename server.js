aa4ee24bd1ce803f1a8e4c9867469737963fa8bd
Craig Fay <craigfay1@gmail.com>
2019-06-05 16:37:48 -0500

    Serve static files and prepare to make admin requests

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
+// env variables
+const { SHOPIFY_API_SECRET_KEY, SHOPIFY_API_KEY, API_VERSION, TUNNEL_URL } = require('./env.json');
+
+/**
+ * Dependencies
+ */
 require('isomorphic-fetch');
 const Koa = require('koa');
 const next = require('next');
 const { default: createShopifyAuth } = require('@shopify/koa-shopify-auth');
-const dotenv = require('dotenv');
 const { verifyRequest } = require('@shopify/koa-shopify-auth');
 const session = require('koa-session');
 const Router = require('koa-router');
+const fs = require('fs');
 
-dotenv.config();
 const { default: graphQLProxy } = require('@shopify/koa-shopify-graphql-proxy');
 const { ApiVersion } = require('@shopify/koa-shopify-graphql-proxy');
 
@@ -16,12 +21,37 @@ const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
-// env variables
-const { SHOPIFY_API_SECRET_KEY, SHOPIFY_API_KEY, API_VERSION, TUNNEL_URL } = process.env;
+function fetchOptions(ctx) {
+  return {
+    credentials: 'include',
+    headers: {
+      'X-Shopify-Access-Token': ctx.session.accessToken,
+      'Content-Type': 'application/json',
+    },
+  };
+}
 
-async function makeAdminRequest(ctx) {
-  console.log('shop:', ctx.session.shop);
-  return ctx.body = { shop: 'hello' };
+async function GET_script_tags(ctx) {
+  const endpoint = `https://${ctx.session.shop}/admin/api/${API_VERSION}/script_tags.json`;
+  const response = await fetch(endpoint, fetchOptions(ctx));
+  const text = await response.text();
+  return ctx.body = text;
+}
+
+async function POST_script_tags(ctx) {
+  const endpoint = `https://${ctx.session.shop}/admin/api/${API_VERSION}/script_tags.json`;
+  const response = await fetch(endpoint, { method: 'POST', ...fetchOptions(ctx) });
+  const text = await response.text();
+  return ctx.body = text;
+}
+
+/**
+ * Serve static files
+ * @param {*} path 
+ * @param {*} ctx 
+ */
+async function static(path, ctx) {
+  await fsp.readFile()
 }
 
 app.prepare().then(() => {
@@ -31,12 +61,10 @@ app.prepare().then(() => {
   const router = new Router();
 
   // Authentication
-  server.use(session(server));
   server.keys = [SHOPIFY_API_SECRET_KEY];
-
-  router.get('/', makeAdminRequest);
+  server.use(session(server));
   server.use(router.routes());
-
+  server.use(require('koa-static')(__dirname + '/static'));
   server.use(
     createShopifyAuth({
       apiKey: SHOPIFY_API_KEY,
@@ -57,24 +85,12 @@ app.prepare().then(() => {
       },
     }),
   );
-
   server.use(graphQLProxy({version: ApiVersion.April19}))
   server.use(verifyRequest());
   server.use(async (ctx) => {
-
     switch(ctx.url) {
-      case '/get-scripts':
-        const options = {
-          credentials: 'include',
-          headers: {
-            'X-Shopify-Access-Token': ctx.session.accessToken,
-            'Content-Type': 'application/json',
-          },
-        };
-        const endpoint = `https://${ctx.session.shop}/admin/api/${API_VERSION}/script_tags.json`;
-        const response = await fetch(endpoint, options);
-        const text = await response.text();
-        return ctx.body = text;
+      case '/get-scripts': return GET_script_tags(ctx);
+      case '/post-scripts': return POST_script_tags(ctx);
       default:
         // Views
         await handle(ctx.req, ctx.res);
